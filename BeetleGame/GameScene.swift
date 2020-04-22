49e83e444a2649cd349de8f91d02faba56b446cb
Adriano Rodrigues Vieira <adriano.antoniev@outlook.com>
2020-04-22 17:34:53 -0300

    Define game physics

--- BeetleGame/GameScene.swift
+++ BeetleGame/GameScene.swift
@@ -9,7 +9,26 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var isGameStarted: Bool = false
+    var isDied: Bool = false
+    let coinSound = SKAction.playSoundFileNamed("CoinSound.mp3", waitForCompletion: false)
+    
+    var score: Int = 0
+    var scoreLabel = SKLabelNode()
+    var highScoreLabel = SKLabelNode()
+    var taptopPlayLabel = SKLabelNode()
+    var restartButton = SKSpriteNode()
+    var pauseButton = SKSpriteNode()
+    var logoImage = SKSpriteNode()
+    var wallPair = SKNode()
+    var moveAndRemove = SKAction()
+    
+    //Criando o passaro para a animacao
+    let birdAtlas = SKTextureAtlas(named: "player")
+    var birdSprites = Array<Any>()
+    var bird = SKSpriteNode()
+    var repeateActionBir = SKAction()
     
     override func didMove(to view: SKView) {
         
@@ -21,5 +40,18 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         
+    }
+    
+    // fazendo os preparativos para criar a cena
+    func createScene() {
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        self.physicsBody?.categoryBitMask = CollisionBitMask.GROUND_CATEGORY
+        self.physicsBody?.collisionBitMask = CollisionBitMask.BIRD_CATEGORY
+        self.physicsBody?.contactTestBitMask = CollisionBitMask.BIRD_CATEGORY
+        self.physicsBody?.isDynamic = false
+        self.physicsBody?.affectedByGravity = false
+        
+        self.physicsWorld.contactDelegate = self
+        self.backgroundColor = SKColor(red: 80.0/255.0, green: 192.0/255.0, blue: 203.0/255.0, alpha: 1.0)
     }
 }
